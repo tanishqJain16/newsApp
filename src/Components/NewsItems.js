32c6e7fc2c1cc5588ac925f44c2ad0544a90531a
Tanishq Jain <tanishqjain1652003@gmail.com>
2022-07-06 22:48:36 +0530

    make it better

--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -6,7 +6,7 @@ export class NewsItems extends Component {
         let { title, discription, imageUrl,newsUrl,author,date} = this.props;
         return (
             <div className='my-3'>
-                <div className="card" >
+                <div className="card">
                     <img src={imageUrl?imageUrl:'https://media.istockphoto.com/photos/breaking-news-world-news-with-map-backgorund-picture-id1182477852?k=20&m=1182477852&s=612x612&w=0&h=I3wdSzT_5h1y9dHq_YpZ9AqdIKg8epthr8Guva8FkPA='} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
